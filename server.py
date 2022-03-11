9e626c42dcd55ee15338adad3e08ceae24278fb9
Piotr Maślanka <pmaslanka@smok.co>
2022-03-11 14:32:59 +0100

    refactored server.py

--- server.py
+++ server.py
@@ -17,6 +17,28 @@ serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 PORT = 8000
 SERVER = '53.158.66.69'
 
+class ClientThread(threading.Thread):
+    def __init__(self, sock):
+        super().__init__(daemon=True)
+        self.socket = sock
+
+    def run(self):
+        data_no = 1
+        while True:
+            data_from_client = clientConnected.recv(1024)
+
+            print(data_from_client.decode())
+
+            try:
+                cur.execute('INSERT INTO obtained_data (data_no, data_value) VALUES (?, ?)',
+                            (data_no, data_from_client.decode()))
+            except sqlite3.IntegrityError:
+                cur.execute('UPDATE obtained_data SET data_from_client.decode()=? WHERE data_no=?',
+                            (data_no, data_from_client.decode()))
+    
+            data_no += 1
+        
+
 serverSocket.bind((SERVER, PORT))
 
 serverSocket.listen()
@@ -25,18 +47,4 @@ while True:
     (clientConnected, clientAddress) = serverSocket.accept()
 
     print("Accepted connection request from %s: %s" % (clientAddress[0], clientAddress[1]))
-
-    data_no = 1
-    while True:
-        data_from_client = clientConnected.recv(1024)
-
-        print(data_from_client.decode())
-
-        try:
-            cur.execute('INSERT INTO obtained_data (data_no, data_value) VALUES (?, ?)',
-                        (data_no, data_from_client.decode()))
-        except sqlite3.IntegrityError:
-            cur.execute('UPDATE obtained_data SET data_from_client.decode()=? WHERE data_no=?',
-                        (data_no, data_from_client.decode()))
-
-        data_no += 1
+    ClientThread(clientConnected).start()
