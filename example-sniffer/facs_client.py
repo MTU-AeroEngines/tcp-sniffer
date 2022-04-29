881d28323a3d8cf5b980794e8011d8873f2e0d64
Kamila Jakubik <92453857+kamilajakubik@users.noreply.github.com>
2022-04-29 07:55:51 +0200

    Update facs_client.py

--- example-sniffer/facs_client.py
+++ example-sniffer/facs_client.py
@@ -1,16 +1,36 @@
 import socket
-import time
+import threading
+import logging
+
+logger = logging.getLogger(__name__)
 
 LOCAL_BIND_ADDR = ('127.0.0.1', 8000)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(LOCAL_BIND_ADDR)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(LOCAL_BIND_ADDR)
 
-for i in range(1, 6):
-    data = f"Message number {i}"
-    client_socket.sendall(data.encode())
-    print(data)
-    time.sleep(1)
+logger.warning('[NEW CONNECTION] Connected to the server')
 
-client_socket.close()
 
+def send_data():
+    threading.Timer(5.0, send_data).start()
+    data = "Message from client"
+    server.sendall(data.encode())
+
+
+def receive():
+    while True:
+        try:
+            data_from_server = server.recv(1024)
+            if not data_from_server:
+                raise OSError('Socket error!')
+            logger.warning('<SERVER> %s', data_from_server.decode())
+        except (socket.error, IOError, OSError):
+            logger.warning('[DISCONNECT] Connection closed\n')
+            server.close()
+            break
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+send_data()
